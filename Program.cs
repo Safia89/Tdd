f6c8013f0524f84c7ded344a3efec898fcacf752
Safia SLIMANI <safia_slimani@hotmail.com>
2019-09-23 10:25:10 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ecrire un programme qui retourne les entiers de 1 à 100
+            // Règles:
+            // pour les multiples de 3, remplacer le nombre par Yop
+            //pour les mutiples de 5 , remplacer le nombre par Yip
+            //pour les multiple de 15, remplacer le nombre par YipYop
+        }
+    }
+}
